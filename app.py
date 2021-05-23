1e88b5961460f7b8cdc69537150fd74887506001
amarthya03 <amarthya10@gmail.com>
2021-05-23 15:54:08 +0530

    Trend Detection Prototype

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, redirect, url_for, request, render_template
-import model
+import pickle
+# import model
+
+
+detection_model = pickle.load(open('classifier.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -24,17 +28,27 @@ posts = [
 def index():
     return render_template("index.html")
 
-@app.route('/trend', methods=['GET', 'POST'])
+@app.route('/trend', methods=['GET'])
 def trend():
     return render_template("trend.html")
 
-@app.route('/send', methods=['GET', 'POST'])
-def predict():
-    if request.method== 'POST':
-        prediction = predict_price()
+
+@app.route('/trend', methods=['POST'])
+def detect_trend():
+    rsi = request.form['rsi']
+    crc = request.form['crc']
+    obv = request.form['obv']
+    vwap = request.form['vwap']
+    res = detection_model.predict([[rsi, crc, obv, vwap]])
+    return render_template('detect_trend.html', data=res)
+
+# @app.route('/send', methods=['GET', 'POST'])
+# def predict():
+#     if request.method== 'POST':
+#         prediction = predict_price()
     
-    if prediction!=0:
-        return render_template("index.html", prediction = prediction)
+#     if prediction!=0:
+#         return render_template("index.html", prediction = prediction)
 
 @app.route('/visualize', methods=['GET', 'POST'])
 def visualize():
